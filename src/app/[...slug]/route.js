22152959b361927efa5282f79ea3c52240a08907
tnGJaTKZQC <dAn0EcugSQ@tutamail.com>
2025-08-06 13:41:31 -0700

    sd

--- src/app/[...slug]/route.js
+++ src/app/[...slug]/route.js
@@ -1,21 +1,25 @@
-import { handleProxy } from "../../utils/proxyHandler";
+import { handleProxy, handlePreflight } from "../../utils/proxyHandler";
 
 export const dynamic = "force-dynamic"; // Disable static caching
 
 export async function GET(req, { params }) {
-  return handleProxy(req, { slug: params.slug });
+  const data = await params;
+  return handleProxy(req, { slug: data.slug });
 }
 
 export async function POST(req, { params }) {
-  return handleProxy(req, { slug: params.slug });
+  const data = await params;
+  return handleProxy(req, { slug: data.slug });
 }
 
 export async function PUT(req, { params }) {
-  return handleProxy(req, { slug: params.slug });
+  const data = await params;
+  return handleProxy(req, { slug: data.slug });
 }
 
 export async function DELETE(req, { params }) {
-  return handleProxy(req, { slug: params.slug });
+  const data = await params;
+  return handleProxy(req, { slug: data.slug });
 }
 
 export async function OPTIONS(req) {
